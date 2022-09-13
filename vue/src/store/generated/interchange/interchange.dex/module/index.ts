6c19e16be62022fb15378dfb67c9ffbbeb97067c
darko.deuric <darko.deuric@ethernal.tech>
2022-09-13 15:47:42 +0200

    Define denom trace, store the voucher

--- vue/src/store/generated/interchange/interchange.dex/module/index.ts
+++ vue/src/store/generated/interchange/interchange.dex/module/index.ts
@@ -4,17 +4,17 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgCancelBuyOrder } from "./types/dex/tx";
 import { MsgSendCreatePair } from "./types/dex/tx";
 import { MsgSendSellOrder } from "./types/dex/tx";
-import { MsgCancelBuyOrder } from "./types/dex/tx";
 import { MsgSendBuyOrder } from "./types/dex/tx";
 import { MsgCancelSellOrder } from "./types/dex/tx";
 
 
 const types = [
+  ["/interchange.dex.MsgCancelBuyOrder", MsgCancelBuyOrder],
   ["/interchange.dex.MsgSendCreatePair", MsgSendCreatePair],
   ["/interchange.dex.MsgSendSellOrder", MsgSendSellOrder],
-  ["/interchange.dex.MsgCancelBuyOrder", MsgCancelBuyOrder],
   ["/interchange.dex.MsgSendBuyOrder", MsgSendBuyOrder],
   ["/interchange.dex.MsgCancelSellOrder", MsgCancelSellOrder],
   
@@ -49,9 +49,9 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgCancelBuyOrder: (data: MsgCancelBuyOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgCancelBuyOrder", value: MsgCancelBuyOrder.fromPartial( data ) }),
     msgSendCreatePair: (data: MsgSendCreatePair): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendCreatePair", value: MsgSendCreatePair.fromPartial( data ) }),
     msgSendSellOrder: (data: MsgSendSellOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendSellOrder", value: MsgSendSellOrder.fromPartial( data ) }),
-    msgCancelBuyOrder: (data: MsgCancelBuyOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgCancelBuyOrder", value: MsgCancelBuyOrder.fromPartial( data ) }),
     msgSendBuyOrder: (data: MsgSendBuyOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgSendBuyOrder", value: MsgSendBuyOrder.fromPartial( data ) }),
     msgCancelSellOrder: (data: MsgCancelSellOrder): EncodeObject => ({ typeUrl: "/interchange.dex.MsgCancelSellOrder", value: MsgCancelSellOrder.fromPartial( data ) }),
     
